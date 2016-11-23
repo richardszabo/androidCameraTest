ddc10d9ad43cf1bea700b40b75a82ea40e099b7a
Rics <richard_szabo@jataka.hu>
2016-11-23 14:38:39 +0100

    Adding textfield for image location. Removing debug messages.

--- app/src/main/java/hu/rics/cameratest/CameraTest.java
+++ app/src/main/java/hu/rics/cameratest/CameraTest.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -38,17 +39,20 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
     boolean previewRunning;
     static final int FOTO_MODE = 0;
     private static final String TAG = "CameraTest";
+    File sdcardLocation;
+    String defaultName = "CameraTest.jpg";
+    EditText imageLocationTextField;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        Log.e(TAG, "onCreate");
-        showMessage("Hello toast 1!");
-
-        saveDataToSDFile("t.txt","hello".getBytes());
 
         setContentView(R.layout.main);
+        sdcardLocation = Environment.getExternalStorageDirectory();
+        imageLocationTextField = (EditText) findViewById(R.id.imageLocation);
+        File imageLocation = new File(sdcardLocation,defaultName);
+        imageLocationTextField.setText(imageLocation.getAbsolutePath());
 
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +64,6 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
                 requestWindowFeature(Window.FEATURE_NO_TITLE);
                 getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
-                Log.e(TAG, "onClick");
                 setContentView(R.layout.camera);
                 final Button cheese = (Button) findViewById(R.id.cheese);
                 surfaceView = (SurfaceView) findViewById(R.id.surface_camera);
@@ -73,12 +76,10 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
     }
 
     public void surfaceCreated(SurfaceHolder sh) {
-        Log.e(TAG, "surfaceCreated");
         camera = Camera.open();
     }
 
     public void surfaceChanged(SurfaceHolder holder, int i, int width, int height) {
-        Log.e(TAG, "surfaceChanged1");
         if (previewRunning) {
             camera.stopPreview();
         }
@@ -88,14 +89,12 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
         //p.setPreviewFormat(PixelFormat.JPEG);
         camera.setParameters(p);
 
-        Log.e(TAG, "surfaceChanged2");
         try {
             camera.setPreviewDisplay(holder);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        Log.e(TAG, "surfaceChanged3");
         camera.startPreview();
         previewRunning = true;
     }
@@ -107,9 +106,6 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
     }
 
     public void onClick(View v) {
-        Log.e(TAG, "onClick2");
-        showMessage("Hello toast 2!");
-        saveDataToSDFile("t2.txt","hello".getBytes());
         camera.takePicture(null, null, mPictureCallback);
     }
 
@@ -125,8 +121,7 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
         FileOutputStream fOut = null;
         try {
             if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-                File sdcard = Environment.getExternalStorageDirectory();
-                File file = new File(sdcard.getAbsolutePath(), filename);
+                File file = new File(filename);
                 fOut = new FileOutputStream(file);
                 fOut.write(data);
                 fOut.flush();
@@ -149,14 +144,12 @@ public class CameraTest extends Activity implements SurfaceHolder.Callback, View
     Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
 
         public void onPictureTaken(byte[] imageData, Camera c) {
-            Log.e(TAG, "onPictureTaken");
-            showMessage("Hello toast 3!");
 
             if (imageData != null) {
 
                 Intent mIntent = new Intent();
 
-                saveDataToSDFile("CameraTest2.jpg",imageData);
+                saveDataToSDFile(imageLocationTextField.getText().toString(),imageData);
 
                 setResult(FOTO_MODE, mIntent);
                 finish();
