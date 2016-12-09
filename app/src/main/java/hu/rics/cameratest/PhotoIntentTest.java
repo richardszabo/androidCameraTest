78a827e12d80cf82ff79aff7e0e043190dbe3c9e
Rics <richard_szabo@jataka.hu>
2016-12-09 15:22:11 +0100

    Photo and video saving works on Android 4 (API 17).
    - For video a workaround was necessary because adding extra MediaStore.EXTRA_OUTPUT to intent did save the image but the camera program crashed.
    The workaround saves the video file in the onActivityResult.
    - All dynamic permission handling (Android 6) was removed for the time being.
    - PhotoIntentTest returns to menu.

--- app/src/main/java/hu/rics/cameratest/PhotoIntentTest.java
+++ app/src/main/java/hu/rics/cameratest/PhotoIntentTest.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,6 +18,8 @@ import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -27,7 +30,7 @@ import static android.os.Environment.DIRECTORY_PICTURES;
 import static android.os.Environment.getExternalStoragePublicDirectory;
 
 /**
- * Created by rics on 2016.11.28..
+ * Created by rics on 2016.11.28.
  */
 
 public class PhotoIntentTest extends Activity {
@@ -35,22 +38,11 @@ public class PhotoIntentTest extends Activity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     boolean isPublic;
     boolean isVideo;
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.CAMERA,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
+    File photoFile;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
-        ActivityCompat.requestPermissions(
-                this,
-                PERMISSIONS_STORAGE,
-                REQUEST_EXTERNAL_STORAGE
-        );
 
         isPublic = getIntent().getBooleanExtra("isPublic",false);
         Log.i(CameraTest.TAG,"isPublic:" + isPublic);
@@ -64,7 +56,7 @@ public class PhotoIntentTest extends Activity {
         // Ensure that there's a camera activity to handle the intent
         if (takeCameraIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
-            File photoFile = null;
+            photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
@@ -73,23 +65,23 @@ public class PhotoIntentTest extends Activity {
             }
             // Continue only if the File was successfully created
             if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "hu.rics.cameratest.fileprovider",
-                        photoFile);
-                // the following block is necessary otherwise intent cannot save photo
-                // solution taken from here: http://stackoverflow.com/a/18332000/21047
-                List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takeCameraIntent, PackageManager.MATCH_DEFAULT_ONLY);
-                for (ResolveInfo resolveInfo : resInfoList) {
-                    String packageName = resolveInfo.activityInfo.packageName;
-                    grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                if( !isVideo ) { // for videos on API 17 MediaStore.EXTRA_OUTPUT crashes so for videos I use saveVideoFile instead
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "hu.rics.cameratest.fileprovider",
+                            photoFile);
+                    // the following block is necessary otherwise intent cannot save photo
+                    // solution taken from here: http://stackoverflow.com/a/18332000/21047
+                    List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takeCameraIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                    for (ResolveInfo resolveInfo : resInfoList) {
+                        String packageName = resolveInfo.activityInfo.packageName;
+                        grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    }
+                    takeCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 }
-                takeCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takeCameraIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
-
-    String mCurrentPhotoPath;
 
     private File createImageFile() throws IOException {
         // Create an image file name
@@ -104,13 +96,33 @@ public class PhotoIntentTest extends Activity {
                 storageDir
         );
 
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void saveVideoFile(Intent intent) {
+        try {
+            AssetFileDescriptor videoAsset = getContentResolver().openAssetFileDescriptor(intent.getData(), "r");
+            FileInputStream fis = videoAsset.createInputStream();
+            FileOutputStream fos = new FileOutputStream(photoFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                fos.write(buffer, 0, length);
+            }
+            fis.close();
+            fos.close();
+        } catch (IOException e) {
+            Log.e(CameraTest.TAG,e.getMessage(),e);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         Log.i(CameraTest.TAG,"requestCode:" + requestCode + " resultCode:" + resultCode);
+        if ( isVideo && resultCode == RESULT_OK ) {
+            saveVideoFile(intent);
+        }
+        finish();
     }
 }
