31b382372723eac6b8e8145a9344f08916d48e6e
Rics <richard_szabo@jataka.hu>
2016-12-10 16:43:19 +0100

    Handling dynamic permissions in CameraTest instead of CameraApiTest so media intents are also working.

--- app/src/main/java/hu/rics/cameratest/CameraApiTest.java
+++ app/src/main/java/hu/rics/cameratest/CameraApiTest.java
@@ -41,59 +41,12 @@ public class CameraApiTest extends Activity implements SurfaceHolder.Callback, V
     Camera camera;
     boolean previewRunning;
     static final int FOTO_MODE = 0;
-    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 123;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        // permission check (https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en)
-        List<String> permissionsNeeded = new ArrayList<>();
-
-        final List<String> permissionsList = new ArrayList<>();
-        if (!addPermission(permissionsList, Manifest.permission.CAMERA)) {
-            Log.i(CameraTest.TAG,"permissionsNeeded.add(\"Camera\");");
-            permissionsNeeded.add("Camera");
-        }
-        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            Log.i(CameraTest.TAG,"permissionsNeeded.add(\"Storage\");");
-            permissionsNeeded.add("External storage");
-        }
-
-        Log.i(CameraTest.TAG,"onCreate:" + permissionsList.size() + ":" + permissionsNeeded.size());
-        if (permissionsList.size() > 0) {
-            if (permissionsNeeded.size() > 0) {
-                // Need Rationale
-                String message = "You need to grant access to " + permissionsNeeded.get(0);
-                for (int i = 1; i < permissionsNeeded.size(); i++)
-                    message = message + ", " + permissionsNeeded.get(i);
-                showMessageOKCancel(message,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(CameraApiTest.this,permissionsList.toArray(new String[permissionsList.size()]),
-                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
-                            }
-                        });
-                return;
-            }
-            ActivityCompat.requestPermissions(this,permissionsList.toArray(new String[permissionsList.size()]),
-                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
-            return;
-        }
-
         createSurface();
-    }
-
-    private boolean addPermission(List<String> permissionsList, String permission) {
-        if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
-            permissionsList.add(permission);
-            // Check for Rationale Option
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,permission)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     void createSurface() {
@@ -104,45 +57,6 @@ public class CameraApiTest extends Activity implements SurfaceHolder.Callback, V
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(this)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
-            {
-                Map<String, Integer> perms = new HashMap<>();
-                // Initial
-                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
-                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
-                // Fill with results
-                for (int i = 0; i < permissions.length; i++) {
-                    Log.i(CameraTest.TAG,"i:" + i + ":" + permissions[i] + ":" + grantResults[i] );
-                    perms.put(permissions[i], grantResults[i]);
-                }
-                // Check for ACCESS_FINE_LOCATION
-                if( perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
-                    && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ) {
-                    // All Permissions Granted
-                    createSurface();
-                } else {
-                    // Permission Denied
-                    Toast.makeText(CameraApiTest.this, "Some Permission is Denied", Toast.LENGTH_SHORT)
-                            .show();
-                }
-            }
-            break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
     }
 
     public void surfaceCreated(SurfaceHolder sh) {
--- app/src/main/java/hu/rics/cameratest/CameraTest.java
+++ app/src/main/java/hu/rics/cameratest/CameraTest.java
@@ -6,7 +6,9 @@ package hu.rics.cameratest;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -29,6 +31,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -47,7 +53,8 @@ public class CameraTest extends Activity {
     EditText imageLocationTextField;
     String defaultName = "CameraTest.jpg";
     File sdcardLocation;
-
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 123;
+    boolean hasRights = false;
 
     /** Called when the activity is first created. */
     @Override
@@ -59,13 +66,18 @@ public class CameraTest extends Activity {
         imageLocationTextField = (EditText) findViewById(R.id.imageLocation);
         File imageLocation = new File(sdcardLocation, defaultName);
         imageLocationTextField.setText(imageLocation.getAbsolutePath());
+        if( requestPermission() ) {
+            hasRights = true;
+        }
 
         View.OnClickListener cameraApiListener = new View.OnClickListener() {
 
             public void onClick(View v) {
+            if( hasRights ) {
                 Intent intent = new Intent(CameraTest.this, CameraApiTest.class);
-                intent.putExtra("filename",imageLocationTextField.getText().toString());
+                intent.putExtra("filename", imageLocationTextField.getText().toString());
                 startActivity(intent);
+            }
             }
         };
 
@@ -75,11 +87,13 @@ public class CameraTest extends Activity {
         View.OnClickListener imageIntentListener = new View.OnClickListener() {
 
             public void onClick(View v) {
+            if( hasRights ) {
                 //Log.i(TAG,"id:" + v.getId());
                 Intent intent = new Intent(CameraTest.this, PhotoIntentTest.class);
-                intent.putExtra("isPublic",v.getId() == R.id.imageIntentPublicButton || v.getId() == R.id.videoIntentPublicButton);
-                intent.putExtra("isVideo",v.getId() == R.id.videoIntentPublicButton || v.getId() == R.id.videoIntentLocaleButton);
+                intent.putExtra("isPublic", v.getId() == R.id.imageIntentPublicButton || v.getId() == R.id.videoIntentPublicButton);
+                intent.putExtra("isVideo", v.getId() == R.id.videoIntentPublicButton || v.getId() == R.id.videoIntentLocaleButton);
                 startActivity(intent);
+            }
             }
         };
         final Button imageIntentPublicButton = (Button) findViewById(R.id.imageIntentPublicButton);
@@ -101,5 +115,93 @@ public class CameraTest extends Activity {
 
         Toast toast = Toast.makeText(context, message, duration);
         toast.show();
+    }
+
+    boolean requestPermission() {
+        // permission check (https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en)
+        List<String> permissionsNeeded = new ArrayList<>();
+
+        final List<String> permissionsList = new ArrayList<>();
+        if (!addPermission(permissionsList, Manifest.permission.CAMERA)) {
+            Log.i(CameraTest.TAG,"permissionsNeeded.add(\"Camera\");");
+            permissionsNeeded.add("Camera");
+        }
+        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Log.i(CameraTest.TAG,"permissionsNeeded.add(\"Storage\");");
+            permissionsNeeded.add("External storage");
+        }
+
+        Log.i(CameraTest.TAG,"onCreate:" + permissionsList.size() + ":" + permissionsNeeded.size());
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "You need to grant access to " + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(CameraTest.this,permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+                            }
+                        });
+                return false;
+            }
+            ActivityCompat.requestPermissions(this,permissionsList.toArray(new String[permissionsList.size()]),
+                    REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this,permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+            {
+                Map<String, Integer> perms = new HashMap<>();
+                // Initial
+                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++) {
+                    Log.i(CameraTest.TAG,"i:" + i + ":" + permissions[i] + ":" + grantResults[i] );
+                    perms.put(permissions[i], grantResults[i]);
+                }
+                // Check for ACCESS_FINE_LOCATION
+                if( perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ) {
+                    // All Permissions Granted
+                    hasRights = true;
+                } else {
+                    // Permission Denied
+                    Toast.makeText(CameraTest.this, "Some Permission is Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
